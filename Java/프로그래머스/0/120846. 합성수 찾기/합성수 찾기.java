2947f7e9dd54c643c6e0a572c67bca7a84688d1a
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-09-22 17:01:41 +0900

    [level 0] Title: 합성수 찾기, Time: 0.05 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i%j == 0) {
+                    answer += 1;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
