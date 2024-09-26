c0047e68e071561e38285d62e17320afc44d732f
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-09-26 15:32:26 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.13 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        StringBuilder sb = new StringBuilder(my_string);
+        
+        char temp = sb.charAt(num1);
+        sb.setCharAt(num1, sb.charAt(num2));
+        sb.setCharAt(num2, temp);
+        
+        
+        String answer = sb.toString();
+        return answer;
+    }
+}
