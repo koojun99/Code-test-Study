b2a67550106bf355d241dc2aa6feb58ef2eae5fe
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2024-11-28 11:20:41 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 264 ms, Memory: 21280 KB -BaekjoonHub

---
+++ Java/백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+// LIS 배열
+        int[] lis = new int[n];
+        for (int i = 0; i < n; i++) {
+            lis[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    lis[i] = Math.max(lis[i], lis[j] + 1);
+                }
+            }
+        }
+
+        // LDS 배열 (오른쪽에서 왼쪽으로 감소)
+        int[] lds = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            lds[i] = 1;
+            for (int j = n - 1; j > i; j--) {
+                if (nums[j] < nums[i]) {
+                    lds[i] = Math.max(lds[i], lds[j] + 1);
+                }
+            }
+        }
+
+        // LBS 계산
+        int maxLength = 0;
+        for (int i = 0; i < n; i++) {
+            maxLength = Math.max(maxLength, lis[i] + lds[i] - 1);
+        }
+
+        System.out.println(maxLength);
+    }
+}
+
+
