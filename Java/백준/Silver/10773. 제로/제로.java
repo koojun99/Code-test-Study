56f429080e24db036ada115ccd12328d4d865a83
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2025-09-22 17:41:16 +0900

    [Silver IV] Title: 제로, Time: 704 ms, Memory: 111896 KB -BaekjoonHub

---
+++ Java/백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+        int answer = 0;
+        for (int num : stack) {
+            answer += num;
+        }
+
+        System.out.println(answer);
+    }
+}
