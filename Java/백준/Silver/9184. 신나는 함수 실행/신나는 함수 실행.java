03a9238d198c1391035840c9839712a66ccbde25
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2025-11-17 20:13:38 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 1796 ms, Memory: 81816 KB -BaekjoonHub

---
+++ Java/백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    static int[][][] dp = new int[21][21][21];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if (a == -1 && b == -1 && c == -1) break;
+
+            System.out.printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c));
+        }
+    }
+
+    static int w(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return 1;
+        }
+
+        if (a > 20 || b > 20 || c > 20) {
+            return w(20, 20, 20);
+        }
+
+        if (dp[a][b][c] != 0) {
+            return dp[a][b][c];
+        }
+
+        if (a < b && b < c) {
+            dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+        } else {
+            dp[a][b][c] = w(a - 1, b, c)
+                    + w(a - 1, b - 1, c)
+                    + w(a - 1, b, c - 1)
+                    - w(a - 1, b - 1, c - 1);
+        }
+
+        return dp[a][b][c];
+    }
+}
