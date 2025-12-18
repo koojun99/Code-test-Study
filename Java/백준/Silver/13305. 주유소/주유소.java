3cf16e7ad94b178f73fbf088027dfff5593e20ca
구환준/모건 <120353547+koojun99@users.noreply.github.com>
2025-12-18 16:36:33 +0900

    [Silver III] Title: 주유소, Time: 940 ms, Memory: 183196 KB, Score: 100 point -BaekjoonHub

--- Java/백준/Silver/13305. 주유소/주유소.java
+++ Java/백준/Silver/13305. 주유소/주유소.java
@@ -5,13 +5,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-
-        int answer = 0;
-        int curr = 0;
-        int[] dists = new int[n-1];
+        long answer = 0;
+        int[] dists = new int[n - 1];
         int[] prices = new int[n];
 
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             dists[i] = sc.nextInt();
         }
 
@@ -19,19 +17,15 @@ public class Main {
             prices[i] = sc.nextInt();
         }
 
-        answer += prices[curr]*dists[curr];
+        int minPrice = prices[0];
 
-        for (int i = 1; i < n-1; i++) {
-            if (prices[curr]*dists[i] < prices[i]*dists[i]) {
-                answer += prices[curr]*dists[i];
-            } else {
-                answer += prices[i] * dists[i];
-                curr = i;
+        for (int i = 0; i < n - 1; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
             }
-
+            answer += (long) minPrice * dists[i]; // 반드시 long 변환 필요
         }
 
         System.out.println(answer);
     }
-
 }
